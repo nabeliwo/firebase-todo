1a19a7249ce05a5aff458fbe1911ef79382a75ee
nabeliwo <hiroki.c.watanabe@gmail.com>
2018-03-12 23:32:38 +0900

    setting firebase

---
+++ src/functions/app/app.js
@@ -0,0 +1,14 @@
+import * as functions from 'firebase-functions'
+import next from 'next'
+
+const dev = process.env.NODE_ENV !== 'production'
+const app = next({ dev, conf: { distDir: 'next' } })
+const handle = app.getRequestHandler()
+
+const nextApp = functions.https.onRequest((request, response) => {
+  console.log(`File: ${request.originalUrl}`)
+  console.log('dev:', dev)
+  return app.prepare().then(() => handle(request, response))
+})
+
+export { nextApp }
---
+++ src/functions/index.js
@@ -0,0 +1,12 @@
+import { nextApp as next } from "./app/app"
+
+/*
+Namespace application services with function groups.
+Partially deploy namespaces for independent service updates.
+*/
+
+const app = {
+  next
+}
+
+export { app }
